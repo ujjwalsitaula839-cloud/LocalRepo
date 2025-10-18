6c077ddfc482b5fd98662849f936c1625bc941ab
ujjwalsitaula839-cloud <ujjwalsitaula839@gmail.com>
2025-10-17 22:46:33 -0500

    new update

--- loginform/src/components/login.jsx
+++ loginform/src/components/login.jsx
@@ -9,6 +9,17 @@ import "./login.css";
 
 function Login(){
   const[action,setAction] = useState("Signup")
+  const[name,setName] = useState("");
+  const[email,setEmail] = useState("");
+  const[password,setPassword] =useState("");
+  //test case 
+  // const handleSubmit = () => {
+  //   console.log("Action:", action);
+  //   console.log("Name:", name);
+  //   console.log("Email:", email);
+  //   console.log("Password:", password);
+  // };
+
 
 
   
@@ -19,15 +30,15 @@ function Login(){
           {action ==="Signup" &&(
           <div className="name"> 
             <img src={UserIcon} alt="img"/>
-            <input type="text" placeholder="your name"/>
+            <input type="text" placeholder="your name" onChange={(e) => setName(e.target.value)}/>
           </div>)}
           <div className="email">
             <img src={Email} alt ="email"></img>
-            <input type="email" placeholder="your email"></input>
+            <input type="email" placeholder="your email" onChange={(e) => setEmail(e.target.value)}></input>
           </div>
           <div className="password">
             <img src={Password} alt ="password"></img>
-            <input type="password" placeholder="Password here"></input>
+            <input type="password" placeholder="Password here" onChange={(e) => setPassword(e.target.value)}></input>
             
           </div>
           <div className="forgot">
